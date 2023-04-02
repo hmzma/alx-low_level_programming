d8d50e7a83349f87905059259314266c25e7664d
hamza <hamzaelmazouzi@gmail.com>
2023-04-02 06:58:13 -0700

    miner update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints the string "_putchar"
+ * main - print _putchar
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     _putchar('_');

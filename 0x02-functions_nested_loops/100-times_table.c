b646b11bce7f29c5df583ed6287ae544c89b17e2
hamza <hamzaelmazouzi@gmail.com>
2023-04-02 09:52:38 -0700

    miner update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -47,7 +45,4 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
-}
-}
-}
-
+}}}
